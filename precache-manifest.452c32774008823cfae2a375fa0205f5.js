ae367ee8e508a271f7aa20faebeb04a2ea8acae5
ilhamheryanto <mohamad.heryanto@gdis-world.com>
2020-08-16 16:20:30 +0700

    Updates

---
+++ precache-manifest.452c32774008823cfae2a375fa0205f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ef755452cb193c31cd384743486ed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf9941d9184b90be242",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "a325c0bdcd319082fc7f",
+    "url": "/static/css/main.fbbc5378.chunk.css"
+  },
+  {
+    "revision": "5bf9941d9184b90be242",
+    "url": "/static/js/2.baa23aaf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.baa23aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a325c0bdcd319082fc7f",
+    "url": "/static/js/main.3dab638f.chunk.js"
+  },
+  {
+    "revision": "5e23f566780bcefbcf72",
+    "url": "/static/js/runtime-main.d833bf8f.js"
+  }
+]);
